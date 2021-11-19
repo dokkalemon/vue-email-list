103a4cd5252331eb6b346c3cdc572cd940091eaf
dokkalemon <dottori.roberto@gmail..com>
2021-11-19 15:58:24 +0100

    add function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('JS ok');
+/* 
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+BONUS
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+// API: https://flynn.boolean.careers/exercises/api/random/mail
+
+/* VUE */
+const app = new Vue({
+    el: '#app',
+    data: {
+        emails: [],
+        email: '',
+    },
+
+    created() {
+        this.genEmails()
+    },
+
+    methods: {
+        //mettiamo 10 mail random nell'array
+        genEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(result => {
+                    this.emails.push(result.data.response);
+                })
+            }
+        }
+    }
+})
